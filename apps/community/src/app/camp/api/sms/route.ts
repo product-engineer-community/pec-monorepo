a6f55d90f3054deb7512f3c2a6852da2c70dd561
david718 <hkc7180@gmail.com>
2025-04-17 18:37:28 +0900

    fix: sms api

--- apps/community/src/app/camp/api/sms/route.ts
+++ apps/community/src/app/camp/api/sms/route.ts
@@ -3,23 +3,57 @@ import { SolapiMessageService } from "solapi";
 
 export async function POST(req: Request) {
   // from webhook, get data
-  const data = await req.json();
-  const { to, email } = data;
-  console.log("🚀 ~ POST ~ data:", data);
+  const data: FormbricksWebhookData = await req.json();
+  //TODO: 9기 알림 신청에서의 값 즉, 10기 에는 바꿔줘야함
+  const { camyx1dh1a0zzkgq87qo0os8: to, g5m3b6gh9l5rpatm93p7s3pj: email } =
+    data.data.data;
   const messageService = new SolapiMessageService(
     process.env.SMS_API_KEY || "",
     process.env.SMS_API_SECRET || "",
   );
 
-  return NextResponse.json({
-    message: "success",
-  });
-
   const text = `안녕하세요 ${email}님. PEC 알림 신청 해주셔서 감사합니다. 저는 코치 Boaz 입니다. 혹시 가능하실때 잠깐 전화할 수 있을까요? 캠프에 대해 설명드릴 것이 있어서요`;
 
-  await messageService.send({
-    to,
-    from: "01050562412",
-    text,
-  });
+  try {
+    console.log("🚀 ~ POST ~ to:", to);
+    await messageService.send({
+      to,
+      from: "01050562412",
+      text,
+    });
+    console.log("🚀 ~ POST ~ text:", text);
+
+    return NextResponse.json({
+      message: "success",
+    });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.error();
+  }
+}
+
+interface FormbricksWebhookData {
+  webhookId: string;
+  event: string;
+  data: {
+    id: string;
+    createdAt: string;
+    updatedAt: string;
+    surveyId: string;
+    displayId: string;
+    contact: null;
+    contactAttributes: null;
+    finished: boolean;
+    endingId: null;
+    data: {
+      [key: string]: string;
+    };
+    variables: Record<string, unknown>;
+    ttc: Record<string, number>;
+    notes: unknown[];
+    tags: unknown[];
+    meta: Record<string, unknown>;
+    singleUseId: null;
+    language: null;
+  };
 }
