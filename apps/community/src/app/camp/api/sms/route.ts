c91c7f47bc1625eac3247d646eb65c9196a6fb8d
david718 <hkc7180@gmail.com>
2025-04-17 18:19:54 +0900

    feat: add sms api

--- apps/community/src/app/camp/api/sms/route.ts
+++ apps/community/src/app/camp/api/sms/route.ts
@@ -1,3 +1,4 @@
+import { NextResponse } from "next/server";
 import { SolapiMessageService } from "solapi";
 
 export async function POST(req: Request) {
@@ -9,6 +10,10 @@ export async function POST(req: Request) {
     process.env.SMS_API_KEY || "",
     process.env.SMS_API_SECRET || "",
   );
+
+  return NextResponse.json({
+    message: "success",
+  });
 
   const text = `안녕하세요 ${email}님. PEC 알림 신청 해주셔서 감사합니다. 저는 코치 Boaz 입니다. 혹시 가능하실때 잠깐 전화할 수 있을까요? 캠프에 대해 설명드릴 것이 있어서요`;
 
