c182d25561c12f1fc06b88de438932c8de0e973f
happhee <ghdtj7@naver.com>
2025-05-03 14:36:59 +0900

    feat : 포인트 다룰때 필요한 model 들 구현

---
+++ apps/community/src/entities/point/model/index.ts
@@ -0,0 +1,7 @@
+export {
+  convertPointToEmoji,
+  convertPointToPercent,
+  convertPointToToastMessage,
+  getPointForAction,
+} from "./point";
+export type { PointType } from "./point.type";
---
+++ apps/community/src/entities/point/model/point.ts
@@ -0,0 +1,50 @@
+import { PointType } from "./point.type";
+
+const POINT_POLICIES: Record<PointType, number> = {
+  post: 20,
+  comment: 5,
+  signup: 10,
+};
+
+const POINT_NOTICE_MESSAGE: Record<PointType, string> = {
+  post: "포스트 작성 완료",
+  comment: "댓글 작성 완료",
+  signup: `회원가입 완료`,
+};
+
+const POINT_LEVEL = {
+  1: { emoji: "🌱", point: 50 },
+  2: { emoji: "☘️", point: 150 },
+  3: { emoji: "🍀", point: 230 },
+  4: { emoji: "🎄", point: 350 },
+};
+export const convertPointToToastMessage = (pointType: PointType) => {
+  return `🌟 ${POINT_POLICIES[pointType]} 포인트 적립 🌟${POINT_NOTICE_MESSAGE[pointType]}`;
+};
+
+export const getPointForAction = (pointType: PointType) => {
+  return POINT_POLICIES[pointType];
+};
+
+const getPointLevel = (point: number) => {
+  if (point >= POINT_LEVEL[3].point) return 4;
+  if (point >= POINT_LEVEL[2].point) return 3;
+  if (point >= POINT_LEVEL[1].point) return 2;
+  return 1;
+};
+
+export const convertPointToPercent = (point: number) => {
+  console.log(
+    point,
+    getPointLevel(point),
+    POINT_LEVEL[getPointLevel(point)].point,
+  );
+  return Math.min(
+    100,
+    Math.round((point / POINT_LEVEL[getPointLevel(point)].point) * 100),
+  );
+};
+
+export const convertPointToEmoji = (point: number) => {
+  return POINT_LEVEL[getPointLevel(point)].emoji;
+};
