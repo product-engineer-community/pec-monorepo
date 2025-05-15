e002b3c297c5484953937e79fd707b580bcd7dcc
david718 <hkc7180@gmail.com>
2025-05-15 16:25:54 +0900

    fix: redirect home

--- apps/gateway/next.config.js
+++ apps/gateway/next.config.js
@@ -2,15 +2,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "https://productengineer.info",
-        permanent: false,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
