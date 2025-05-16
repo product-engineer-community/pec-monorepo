1f411024526eb7274bc0af78c54e99c3fdde9055
david718 <hkc7180@gmail.com>
2025-05-16 12:07:05 +0900

    fix: reset

--- packages/auth/src/features/auth/action/index.ts
+++ packages/auth/src/features/auth/action/index.ts
@@ -142,7 +142,7 @@ export async function socialSignIn(
   const next = formData.get("next") as string;
   const supabase = await getSupabaseServerClient();
 
-  const siteUrl = "https://auth.productengineer.info";
+  const siteUrl = getOrigin("auth");
   const baseCallbackUrl = `${siteUrl.replace(/\/$/, "")}${AUTH_CALLBACK_PATHNAME}`;
   const redirectTo = `${baseCallbackUrl}?next=${encodeURIComponent(next)}`;
 
