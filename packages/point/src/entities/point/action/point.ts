20bfecaac8a5b7f0e8382f245426d29259e155ae
david718 <hkc7180@gmail.com>
2025-05-17 16:37:28 +0900

    fix: getUserPoint with supabase server client

--- packages/point/src/entities/point/action/point.ts
+++ packages/point/src/entities/point/action/point.ts
@@ -1,10 +1,8 @@
-import { getSupabaseClient } from "@packages/supabase/src/client";
+"use server";
+import { getSupabaseServerClient } from "@packages/supabase";
 
 export const getUserPoint = async (authorId: string) => {
-  const supabase = getSupabaseClient({
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  });
+  const supabase = await getSupabaseServerClient();
   const { data, error } = await supabase
     .from("points")
     .select("point")
